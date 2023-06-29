1d33d969e95df345f87ce5e26aa675c73330fc72
Kateryna1706 <silckatya@gmail.com>
2023-06-29 13:32:58 +0200

    added localStorage

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -51,9 +51,19 @@ export class App extends Component {
     );
   };
 
-  componentDidMount() {}
+  componentDidMount() {
+    const contacts = localStorage.getItem('contacts');
+    const parsedContacts = JSON.parse(contacts);
+    this.setState({
+      contacts: parsedContacts,
+    });
+  }
 
-  componentDidUpdate() {}
+  componentDidUpdate(prevProps, prevState) {
+    if (this.state.contacts !== prevState.contacts) {
+      localStorage.setItem('contacts', JSON.stringify(this.state.contacts));
+    }
+  }
 
   componentWillUnmount() {}
 
