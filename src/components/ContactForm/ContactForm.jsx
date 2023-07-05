9fff33979193740ef98708b8cc911cffeb891e24
Kateryna1706 <silckatya@gmail.com>
2023-07-05 13:47:14 +0200

    fixed

--- src/components/ContactForm/ContactForm.jsx
+++ src/components/ContactForm/ContactForm.jsx
@@ -1,4 +1,3 @@
-// import { useState } from 'react';
 import css from './ContactForm.module.css';
 import PropTypes from 'prop-types';
 import { Formik, Form, Field, ErrorMessage } from 'formik';
@@ -8,44 +7,17 @@ const initialValues = { name: '', number: '' };
 
 const userSchema = Yup.object().shape({
   name: Yup.string().min(2).max(70).required(),
-  number: Yup.number().required(),
+  number: Yup.number().positive().integer().required(),
 });
 
 export const ContactForm = ({ onSubmit }) => {
-  // const [name, setName] = useState('');
-  // const [number, setNumber] = useState('');
-
-  // const handleChange = event => {
-  //   const { name, value } = event.currentTarget;
-
-  //   switch (name) {
-  //     case 'name':
-  //       setName(value);
-  //       break;
-
-  //     case 'number':
-  //       setNumber(value);
-  //       break;
-
-  //     default:
-  //       return;
-  //   }
-  // };
-
   const handleSubmit = (values, actions) => {
-    // event.preventDefault();
     const { name, number } = values;
     console.log(values.name);
     console.dir(actions);
     onSubmit(name, number);
     actions.resetForm();
-    // reset();
   };
-
-  // const reset = () => {
-  //   setName('');
-  //   setNumber('');
-  // };
 
   return (
     <Formik
@@ -59,9 +31,7 @@ export const ContactForm = ({ onSubmit }) => {
           <Field
             type="text"
             name="name"
-            // value={name}
             title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
-            // onChange={handleChange}
           />
           <ErrorMessage name="name" component="div" />
         </label>
@@ -70,9 +40,7 @@ export const ContactForm = ({ onSubmit }) => {
           <Field
             type="tel"
             name="number"
-            // value={number}
             title="Phone number must be digits and can contain spaces, dashes, parentheses and can start with +"
-            // onChange={handleChange}
           />
           <ErrorMessage name="number" component="div" />
         </label>
