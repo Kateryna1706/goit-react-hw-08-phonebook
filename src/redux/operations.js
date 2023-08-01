b116c163013bee8fbc25dd4704316ccd16a771ee
Kateryna1706 <silckatya@gmail.com>
2023-08-01 15:15:43 +0200

    added createAsyncThunk

--- src/redux/operations.js
+++ src/redux/operations.js
@@ -10,6 +10,7 @@ export const fetchContacts = createAsyncThunk(
       const response = await axios.get('/contacts');
       return response.data;
     } catch (error) {
+      
       return thunkAPI.rejectWithValue(error.message);
     }
   }
